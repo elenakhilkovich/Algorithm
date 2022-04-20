be50904d6dc1145ff7e321e1d957b9bb88c4f979
Елена Хилькович <elenakhilkovich@gmail.com>
2022-04-20 22:06:06 +0300

    init commit

---
+++ src/main/java/com/example/algor/AlgorApplication.java
@@ -0,0 +1,15 @@
+package com.example.algor;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class AlgorApplication {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
